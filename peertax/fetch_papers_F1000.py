b2817d528602bb4d8167fc35821a6d2b7f816d6e
Alessio Caciagli <ac2014@cam.ac.uk>
2019-07-18 22:11:08 +0100

    Added html with F1000 paper URLs

--- peertax/fetch_papers_F1000.py
+++ peertax/fetch_papers_F1000.py
@@ -1,25 +1,23 @@
+import pathlib
+import re
 from bs4 import BeautifulSoup
 import requests
 from tqdm import tqdm_notebook as tqdm
-import re
-import pathlib
 
-#URLs fetching
+# URLs fetching
 urls = []
-idx = [1,4,5,6]
+idx = [1, 4, 5, 6]
 with open("../published-xml-urls.html", "r") as f:
     contents = f.read()
     soup = BeautifulSoup(contents, 'lxml')
-    for i,link in enumerate(soup.find_all('a')):
+    for i, link in enumerate(soup.find_all('a')):
         xml_file = link.get('href')
         urls.append(xml_file)
 
-#Downloading papers
+# Downloading papers
 out_folder = '../f1000_articles/'
-pathlib.Path(out_folder).mkdir(exist_ok=True) 
-for url in tqdm(urls):    
-        r = requests.get(url)
-        name = '-'.join([re.split('\W+', url)[_] for _ in idx])+'.xml'
-        open(out_folder+name, 'wb').write(r.content)
-
-
+pathlib.Path(out_folder).mkdir(exist_ok=True)
+for url in tqdm(urls):
+    r = requests.get(url)
+    name = '-'.join([re.split(r'\W+', url)[_] for _ in idx])+'.xml'
+    open(out_folder+name, 'wb').write(r.content)
