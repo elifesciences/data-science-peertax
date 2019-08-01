bb4fd2a79e49ddc19eed862346126ef30871b21c
Alessio Caciagli <ac2014@cam.ac.uk>
2019-08-01 16:30:48 +0100

    Updated sentencier and added wellcome reviews

---
+++ peertax/extract_reviews_from_xml_wellcome.py
@@ -0,0 +1,38 @@
+# Configuration
+import os
+import re
+import pandas as pd
+from lxml import etree
+from tqdm import tqdm
+import xml_custom as xml
+
+paper_folder = '../data/articles/wellcome_articles/'
+# paper_folder = os.path.abspath(paper_folder) + '/'
+
+# Extract reviews
+paper_ID = []
+review_ID = []
+review = []
+for doc in tqdm(os.listdir(paper_folder)):
+    if not doc.endswith('.xml'):
+        continue
+    fullname = os.path.join(paper_folder, doc)
+    if os.stat(fullname).st_size != 0:
+        tree = etree.parse(fullname)
+        # Select article DOI
+        ID = tree.findall("//article-meta/article-id[@pub-id-type='doi']")
+        # Select reviews
+        rev_ID = tree.findall("//sub-article/front-stub/article-id[@pub-id-type='doi']")
+        rev_text = tree.findall("//sub-article[@article-type='ref-report']/body")
+        for i, _ in enumerate(rev_text):
+            paper_ID.append(re.sub(r'\t+', '', xml.get_text_content(ID[0])))
+            review_ID.append(re.sub(r'\t+', '', xml.get_text_content(rev_ID[i])))
+            review.append(re.sub(r'\t+', '', xml.get_text_content(rev_text[i])))
+
+# Put reviews in a dataframe
+df = pd.DataFrame({'manuscript_ID': paper_ID,
+                   'review_ID': review_ID,
+                   'review': review})
+
+# Save the dataframe
+df.to_csv('../data/reviews/wellcome_reviews.csv')
--- peertax/sentencizer_LDA.py
+++ peertax/sentencizer_LDA.py
@@ -29,17 +29,22 @@ from spacy.pipeline import Sentencizer
 def custom_sentencizer(texts):
     # disabling Named Entity Recognition for speed
     nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser'])
-    boundary_1 = re.compile('(fig)')
-    boundary_2_2 = re.compile('(et)')
-    boundary_2_1 = re.compile('(al)')
-    boundary_3 = re.compile('(ex)')
+    boundary_1 = re.compile(r'\b(fig)\b|\b(ex)\b|\b[a-z]\b|\b(prof)\b|'
+                            r'\b(eg)\b|\b(etc)\b|\b(sp)\b|\b(an)\b|'
+                            r'\b(pp)\b|\b(vol)\b|\b(col)\b')
+    boundary_2_2 = re.compile(r'\b(et)\b')
+    boundary_2_1 = re.compile(r'\b(al)\b')
+    boundary_p = re.compile(r'[.?!:;’“”\"\'0-9]|(wAnM.)')
+    boundary_a = re.compile(r'\b(th)\b|\b(st)\b|\b(nd)\b|\b(rd)\b|[-]')
+    boundary_d = re.compile(r'[0-9]')
 
     # Utility functions
     def custom_seg_1(doc):
         prev = doc[0].text
         length = len(doc)
         for index, token in enumerate(doc):
-            if (token.text == '.' and boundary_1.match(prev.lower()) and index != (length - 1)):
+            if (token.text == '.' and boundary_1.match(prev.lower())
+                    and index != (length - 1)):
                 doc[index+1].sent_start = False
             prev = token.text
         return doc
@@ -55,12 +60,12 @@ def custom_sentencizer(texts):
             prev_1 = doc[1].text
             for index, token in enumerate(doc):
                 if index > 0:
-                    if (((token.text == '.') | (token.text == '.,'))
+                    if (((token.text == '.') | (token.text == '.,') | (token.text == '.('))
                             and boundary_2_2.match(prev_2.lower())
                             and boundary_2_1.match(prev_1.lower())
                             and index != (length - 1)):
 
-                        if doc[index+1].text == ',':
+                        if ((doc[index+1].text == ',') or (doc[index+1].text == '(')):
                             doc[index+2].sent_start = False
                         else:
                             doc[index+1].sent_start = False
@@ -73,9 +78,33 @@ def custom_sentencizer(texts):
         prev = doc[0].text
         length = len(doc)
         for index, token in enumerate(doc):
-            if (token.text == '.' and boundary_3.match(prev.lower()) and index != (length - 1)):
+            if ((token.text == '\n' or token.text == '\n ')
+                    and not boundary_p.match(prev.lower()) and index != (length - 1)):
                 doc[index+1].sent_start = False
             prev = token.text
+        return doc
+
+    def custom_seg_4(doc):
+        succ = doc[1].text
+        length = len(doc)
+        for index, token in enumerate(doc):
+            if index < (length-2):
+                if ((token.text == '\n' or token.text == '\n ') and boundary_a.match(succ.lower())):
+                    doc[index+1].sent_start = False
+                succ = doc[index+2].text
+        return doc
+
+    def custom_seg_5(doc):
+        prev = doc[0].text
+        succ = doc[1].text
+        length = len(doc)
+        for index, token in enumerate(doc):
+            if index < (length-2):
+                if ((token.text == '\n' or token.text == '\n ') and boundary_d.match(prev.lower())):
+                    if ~succ.isupper():
+                        doc[index+1].sent_start = False
+                prev = token.text
+                succ = doc[index+2].text
         return doc
 
     def brief_cleaning_fun(text):
@@ -85,16 +114,30 @@ def custom_sentencizer(texts):
         text = re.sub(r'\(\?', r'\(', str(text))
         # Remove question marks inside parenthesis (they mess up sentence splitting)
         text = re.sub(r'\?\)', r'\)', str(text))
-        # Remove \xa0 tags (they mess up sentence splitting)
-        text = re.sub(r'[\xa0]', '', str(text))
+        text = re.sub(r'(\? \()', r' \(', str(text))
+        # Replace \xa0\n tags with space
+        text = re.sub(r'(\xa0\n)', ' ', str(text))
+        text = re.sub(r'(\xa0 \n)', ' ', str(text))
+        # Replace -e.g. with e.g.
+        text = re.sub(r'(-e.g.)', 'e.g.', str(text))
+        # Replace \xa0 tags with space
+        text = re.sub(r'(\xa0)', ' ', str(text))
+        # Remove newline characters between brackets.
+        text = re.sub(r'(?s)(?<=[\(]).*?(?=[\)])',
+                      lambda x: x.group().replace('\n', ' '), str(text))
+        # Replace space between linebreak characters
+        text = re.sub(r'(?<=(\n)) *(?=(\n))', '', str(text))
         # Replace multiple occurrences of whitespace characters with single one
         text = re.sub(r'(\s)\1{1,}', r'\1', str(text))
+        # Replace square brackets
+        text = re.sub(r'[\[\]]', '', str(text))
+        # Replace occurrences of newline character before a comma
+        text = re.sub(r'(\n,)|(\n ,)', r',\n', str(text))
+        # keep only certain characters
+        text = re.sub(r"[^a-zA-Z0-9,'‘’“”\":;.?!\(\)\-(\n)]", ' ', str(text))
         return text
 
     def sentence_cleaning_fun(text):
-        # keep only letters, commas, colon, semicolon,
-        # period, question marks, exclamation marks and hyphen
-        text = re.sub(r"[^a-zA-Z0-9,'\":;.?!\(\)\-]", ' ', str(text))
         if not text:
             text = 'nan'
         return text
@@ -105,6 +148,14 @@ def custom_sentencizer(texts):
             text = [sentence_cleaning_fun(token.text).strip().split() for token in doc.sents]
             # Remove short sentences
             text = [' '.join(x) for x in text if len(x) > 1]
+            # Remove bracket characters if they are at the end of a sentence.
+            # Remove space between digit and "st","nd","rd" and "th"
+            for i, x in enumerate(text):
+                if x[-1] in ['(', ')']:
+                    text[i] = x[:-1]
+                text[i] = re.sub(r"(?i)\b[0-9][0-9]*\b (\bst\b|\bnd\b|\brd\b|\bth\b)",
+                                 lambda x: x.group().replace(' ', ''), str(text[i]))
+
             if text:
                 return text
             else:
@@ -122,6 +173,8 @@ def custom_sentencizer(texts):
     nlp.add_pipe(custom_seg_1, after='sentencizer')
     nlp.add_pipe(custom_seg_2, after='custom_seg_1')
     nlp.add_pipe(custom_seg_3, after='custom_seg_2')
+    nlp.add_pipe(custom_seg_4, after='custom_seg_3')
+    nlp.add_pipe(custom_seg_5, after='custom_seg_4')
 
     brief_cleaning = (brief_cleaning_fun(row) for row in texts)
     texts_processed = [sentencization(doc) for doc in tqdm(nlp.pipe(brief_cleaning, batch_size=25),
